864dd417a3cd74c25c071ff4ed4dd9cc50d15982
taciturn <151200155+emma-yijun-ma@users.noreply.github.com>
2024-03-26 20:55:09 -0700

    Update LCA.py

--- LCA.py
+++ LCA.py
@@ -3,3 +3,12 @@ def lowestCommonAncestor(self, root, p, q):
     left, right = (self.lowestCommonAncestor(kid, p, q)
                    for kid in (root.left, root.right))
     return root if left and right else left or right
+class TreeNode(object):
+
+     def __init__(self, val=0, left=None, right=None):
+
+         self.val = val
+
+         self.left = left
+
+         self.right = right
